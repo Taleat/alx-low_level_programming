c54ce0e0df29105d9918627bff8852a895f90d19
Emmanuel Taleat <emmanueltaleat2003@gmail.com>
2023-02-28 14:35:59 -0800

    at last

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * swap_init - swaps the values of two integers
+ * swap_int - swaps the values of two integers
  * @a: int a
  * @b: int b
  */
-void swap_init(int *a, int *b)
+void swap_int(int *a, int *b)
 {
 	int q;
 
