83ea6ab44995f27201197caf4c8d836425d04f52
Emmanuel Taleat <emmanueltaleat2003@gmail.com>
2023-02-28 16:09:09 -0800

    Task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - to print n elements of an array
+ * @a: array
+ * @n: number of values to be printed
+ */
+
+void print_array(int *a, int n)
+{
+	int m;
+
+	for (m = 0; m < n; m++)
+	{
+		printf("%d", a[m]);
+		if (m != n - 1)
+			printf(", ");
+	}
+	_putchar('\n');
+}
