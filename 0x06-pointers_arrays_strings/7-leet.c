b0455efec9d4016815b680fcbe58981209974d0e
Emmanuel Taleat <emmanueltaleat2003@gmail.com>
2023-03-02 19:50:07 -0800

    Task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @n: input
+ * Return: the value of n
+ */
+
+char *leet(char *n)
+{
+	int c, d;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+
+	for (c = 0; n[c] != '\0'; c++)
+	{
+		for (d = 0; d < 10; d++)
+		{
+			if (n[c] == s1[d])
+			{
+				n[c] = s2[d];
+			}
+		}
+	}
+	return (n);
+}
