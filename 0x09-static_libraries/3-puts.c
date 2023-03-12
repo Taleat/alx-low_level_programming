ef236b723a52c916548edb9d7128cc9b9dd003a0
Emmanuel Taleat <emmanueltaleat2003@gmail.com>
2023-03-11 17:24:44 -0800

    Task 0

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - Entry point
+ * @str: string
+ */
+
+void _puts(char *str)
+
+{
+	int n = 0;
+
+	while (str[n] != '\0')
+	{
+		_putchar(str[n]);
+		n++;
+	}
+	_putchar('\n');
+}
